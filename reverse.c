5532454197275eab324b15524f996d369df1f77e
AnuvratSharma <2024a6r024@mietjammu.in>
2024-11-28 00:53:33 +0530

    Create reverse.c

---
+++ reverse.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(){
+   int rev=0,n;
+    printf("enter the number ");
+    scanf("%d",&n);
+    while (n!=0)
+    {
+        int rem=n%10;
+        rev=rev*10+rem;
+        n=n/10;
+    }
+    printf("Reverse = %d",rev);
+    return 0;
+}                      
